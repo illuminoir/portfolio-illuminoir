84689f98d4831d5b3523e35a11d104bb9c5e1cbf
Allan Khelifi <khelifiallan@gmail.com>
2026-03-12 01:14:02 +0100

    Push static files for github pages

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  basePath: '',
+  assetPrefix: '/',
+  typescript: {
+    ignoreBuildErrors: true,
+  },
 };
 
 export default nextConfig;
